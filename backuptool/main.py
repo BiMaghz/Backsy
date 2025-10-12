142bba62aadfd914a7e8cc7e9a4a5e06737935bb
BiMaghz <9ineamir@gmail.com>
2025-10-12 23:08:55 +0330

    feat(app): Add main application entrypoint and package definition

---
+++ backuptool/main.py
@@ -0,0 +1,36 @@
+import logging
+import sys
+from backuptool.core.logger import setup_logging
+from backuptool.core.config import load_config
+from backuptool.backup_manager import BackupManager
+from backuptool.core.locking import FileLock, LockExistsError
+from backuptool.utils.notifications import send_fatal_alert
+
+def main():
+    setup_logging()
+    config = None
+
+    try:
+        with FileLock("/tmp/Backsy.lock", exit_on_lock=True):
+            config = load_config()
+            manager = BackupManager(config)
+            manager.run()
+            logging.info("Backup process completed successfully.")
+
+    except LockExistsError as e:
+        logging.warning(str(e))
+        sys.exit(1)
+    except (FileNotFoundError, KeyError) as e:
+        error_message = f"Configuration error: {e}"
+        logging.critical(error_message, exc_info=True)
+        send_fatal_alert(error_message, config=None)
+        sys.exit(1)
+        
+    except Exception as e:
+        error_message = f"A fatal, unexpected error occurred: {e}"
+        logging.critical(error_message, exc_info=True)
+        send_fatal_alert(error_message, config=config)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
